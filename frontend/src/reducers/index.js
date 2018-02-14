e7a70b1e89771230be1fdbfc695c703a01b367db
Inacio <inaciogcerqueira@gmail.com>
2018-02-14 17:59:19 -0200

    Create reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,1 +1,24 @@
-index.js
+import { RECEIVE_POSTS, RECEIVE_CATEGORIES } from '../actions';
+
+const initialState = {
+  categories: null,
+  posts: null,
+  comments: null,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case RECEIVE_POSTS:
+      return {
+        ...state,
+        posts: action.posts,
+      };
+    case RECEIVE_CATEGORIES:
+      return {
+        ...state,
+        categories: action.categories,
+      };
+    default: return state;
+  }
+}
+export default reducer;
