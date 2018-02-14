177113f2a314ce1f8178c224d52b80b7f60064bb
Inacio <inaciogcerqueira@gmail.com>
2018-02-14 18:00:45 -0200

    Connect the app component to the store

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,15 @@
+import AppBar from 'material-ui/AppBar';
 import React, { Component } from 'react';
 import { MuiThemeProvider } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
+import { connect } from 'react-redux';
+import { getPosts, getCategories } from '../actions';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(getPosts);
+    this.props.dispatch(getCategories);
+  }
   render() {
     return (
       <div className="App">
@@ -17,4 +23,9 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  categories: Object.keys(state.categories || {}),
+  posts: Object.keys(state.posts || {}).map(postId => state.posts[postId]),
+});
+
+export default connect(mapStateToProps)(App);
