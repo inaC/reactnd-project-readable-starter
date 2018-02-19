b6d399c74fb67985bd6ea374d18aad2796f255d8
Inacio <inaciogcerqueira@gmail.com>
2018-02-18 22:50:29 -0300

    Refactor App component with respect to its application bar

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,8 @@
-import AppBar from 'material-ui/AppBar';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Card, CardHeader, CardActions } from 'material-ui/Card';
 import IconButton from 'material-ui/IconButton';
-import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 import ActionStars from 'material-ui/svg-icons/action/stars';
 import ActionThumbUp from 'material-ui/svg-icons/action/thumb-up';
 import ActionThumbDown from 'material-ui/svg-icons/action/thumb-down';
@@ -9,75 +10,27 @@ import ActionVisibility from 'material-ui/svg-icons/action/visibility';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
 import CommunicationForum from 'material-ui/svg-icons/communication/forum';
 import EditorModeEdit from 'material-ui/svg-icons/editor/mode-edit';
-import ContentSort from 'material-ui/svg-icons/content/sort';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import IconMenu from 'material-ui/IconMenu';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
-
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getPosts, getCategories } from '../actions';
+import ApplicationBar from './ApplicationBar';
 import './App.css';
+import { getPosts } from '../actions';
+
 
 class App extends Component {
   static propTypes = {
-    categories: PropTypes.array.isRequired,
     posts: PropTypes.array.isRequired,
-    dispatch: PropTypes.func.isRequired,
+    getPosts: PropTypes.func.isRequired,
   }
 
-  state = {
-    open: false,
-    category: 'All',
-    sortBy: 'voteScore',
-  }
   componentDidMount() {
-    this.props.dispatch(getPosts);
-    this.props.dispatch(getCategories);
+    this.props.getPosts();
   }
-
-  setCategory = category => this.setState({ category })
-  setSortBy = type => this.setState({ sortBy: type })
-  toggleDrawer = () => this.setState({ open: !this.state.open })
 
   render() {
-    const sortByTypesDescription = { voteScore: 'Vote Score (biggest to lowest)', createdAt: 'Creation Date (newest to oldest)' };
     return (
       <div className="App">
-        <AppBar
-          title={`${this.state.category} posts`}
-          iconElementLeft={<IconButton tooltip="Categories"><NavigationMenu /></IconButton>}
-          iconElementRight={
-            <IconMenu iconButtonElement={<IconButton tooltip="Sort by"><ContentSort /></IconButton>}>
-              {Object.keys(sortByTypesDescription).map(type => (
-                <MenuItem
-                  key={type}
-                  disabled={this.state.sortBy === type}
-                  onClick={() => this.setSortBy(type)}
-                  primaryText={sortByTypesDescription[type]}
-                />
-              ))}
-            </IconMenu>
-          }
-          onLeftIconButtonClick={this.toggleDrawer}
-        />
-        <Drawer
-          open={this.state.open}
-          docked={false}
-          onRequestChange={open => this.setState({ open })}
-        >
-          {this.props.categories.map(category => (
-            <MenuItem
-              key={category}
-              onClick={() => this.setCategory(category)}
-              disabled={this.state.category === category}
-            > {category}
-            </MenuItem>
-           ))}
-        </Drawer>
+        <ApplicationBar />
         <div className="body">
           {this.props.posts.map(post => (
             <div key={post.id} style={{ marginBottom: '20px' }}>
@@ -121,8 +74,11 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({
-  categories: ['All'].concat(Object.keys(state.categories || {}).map(category => category[0].toUpperCase() + category.slice(1))),
   posts: Object.keys(state.posts || {}).map(postId => state.posts[postId]),
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  getPosts: () => dispatch(getPosts),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
