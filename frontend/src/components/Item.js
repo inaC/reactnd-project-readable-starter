4c6b17eb8f31a5ca8564a3d12b93d50e4f9b8001
Inacio <inaciogcerqueira@gmail.com>
2018-03-03 18:17:18 -0300

    Reuse Item component to make it show the post body when user clicks on viewing details

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, CardHeader } from 'material-ui/Card';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 import PropTypes from 'prop-types';
+import { getPosts, setCategory } from '../actions';
 import ItemActions from './ItemActions';
+import paramTypePresent from '../util/urlParams';
 
 class Item extends Component {
   static propTypes = {
-    id: PropTypes.string.isRequired,
+    id: PropTypes.string,
+    getPosts: PropTypes.func.isRequired,
+    comments: PropTypes.object,
     post: PropTypes.object.isRequired,
     match: PropTypes.object,
+    defaultCategory: PropTypes.string.isRequired,
+    currentCategory: PropTypes.string.isRequired,
+    setCategory: PropTypes.func.isRequired,
   }
+
+  componentDidMount() {
+    if (this.props.match && this.props.post.title === null) this.props.getPosts();
+    if (paramTypePresent(this.props.match, 'category') && this.props.defaultCategory === this.props.currentCategory) {
+      this.props.setCategory(this.props.match.params.category);
+    }
+  }
+
+  renderBody = () => (<CardText> {this.props.post.body} </CardText>)
 
   render() {
     return (
       <Card>
         <CardHeader title={this.props.post.title} subtitle={`@${this.props.post.author}`} />
-        <ItemActions
-          post={this.props.post}
-        />
+        { this.props.match ? this.renderBody() : null }
+        <ItemActions post={this.props.post} />
       </Card>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  post: state.posts[ownProps.match ? ownProps.match.params.post_id : ownProps.id],
+  post: state.posts ? state.posts[ownProps.match ? ownProps.match.params.post_id : ownProps.id] : { title: null, author: null },
+  defaultCategory: state.ui.defaultCategory,
+  currentCategory: state.ui.currentCategory,
+  comments: ownProps.match && state.comments ? state.comments[ownProps.match.params.post_id] : {},
 });
 
-export default connect(mapStateToProps)(Item);
+const mapDispatchToProps = dispatch => ({
+  getPosts: () => dispatch(getPosts),
+  setCategory: category => dispatch(setCategory(category)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Item);
